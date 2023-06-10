dd93eb701d298f90d3b965a66bae704150d619bc
monimoreno38 <monimoreno38@gmail.com>
2023-06-10 10:41:29 -0300

    agrego tp1

---
+++ practico1/frutas.js
@@ -0,0 +1,9 @@
+let frutas = []
+frutas.push("anana", "kiwi", "frutilla", "casis", "mango");
+console.log(frutas);
+
+let ultima = frutas.pop();
+console.log("La ultima fruta agregada fue: " + ultima)
+
+let primera = frutas.shift();
+console.log("la primera fruta agregada fue: " + primera);
---
+++ practico1/miNombreCompleto.js
@@ -0,0 +1,5 @@
+let nombre;
+nombre = "Monica";
+let apellido = "Moreno";
+let edad ="38";
+console.log("Hola, " + nombre + " " + apellido + "." + " Tienes " + edad + " años.");
